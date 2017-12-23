8605118ce2eb09057e2e3a8ef7dc1e0212ddf169
Mr. Dragon <whyorean@gmail.com>
2017-12-24 03:35:13 +0530

    Add standalone build tools

---
+++ build/core/toolchains/aarch64-linux-android-snapdragonclang/config.mk
@@ -0,0 +1,25 @@
+# * Copyright (c) 2017 Qualcomm Technologies, Inc.
+#       All Rights Reserved.
+#       Qualcomm Technologies Inc. Confidential and Proprietary.
+#       Notifications and licenses are retained for attribution purposes only.
+
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# config file for the arm64 Snapdragon LLVM ARM 4.0 toolchain for the Android
+# NDK the real meat is in the setup.mk file adjacent to this one
+#
+TOOLCHAIN_ARCH := arm64
+TOOLCHAIN_ABIS := arm64-v8a
